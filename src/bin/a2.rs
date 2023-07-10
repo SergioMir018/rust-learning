c6802f05400abe06adcd704f2b039a51e9af73d5
Sergio Mir <voidtracer6@gmail.com>
2023-07-10 13:12:12 -0400

    ./scr/bin/a2.rs: solution implemented

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -5,6 +5,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result 
 
-fn main() {
+fn add_numbers(num1: i32, num2: i32) -> i32 {
+     return num1 + num2;
+}
 
+fn display_result(num1: i32, num2: i32) {
+     print!("{:?}",add_numbers(num1, num2))
+}
+
+fn main() {
+     display_result(10, 8);
 }
