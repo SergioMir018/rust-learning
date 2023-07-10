ba888cc458e7e24ad6d4d5adf22c0113804c425e
Sergio Mir <voidtracer6@gmail.com>
2023-07-10 13:09:10 -0400

    ./scr/bin/a2.rs: types added

---
+++ src/bin/a2.rs
@@ -0,0 +1,10 @@
+// Topic: Basic arithmetics
+//
+// Program requirements:
+// * Use a function to add two number together
+// * Use a function to display the result
+// * Use the "{:?}" token in the println macro to display the result 
+
+fn main() {
+
+}
