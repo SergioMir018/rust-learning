2677729ae6829195f1da0eb7725a4c0e6c77335b
Sergio Mir <voidtracer6@gmail.com>
2023-07-10 13:03:10 -0400

    ./scr/bin/numeric-types.rs: added

---
+++ src/bin/numeric-types.rs
@@ -0,0 +1,14 @@
+fn sub(a: i32, b: i32) -> i32 {
+     return a - b;
+}
+
+fn main() {
+     let _sum = 2 + 2;
+     let _value = 10 - 5;
+     let _division = 10 / 2;
+     let _multiplication = 6 * 6;
+
+     let five = sub(8 , 5);
+
+     println!("{:?}", five);
+}
