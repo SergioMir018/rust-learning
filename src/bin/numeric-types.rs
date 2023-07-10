34e900483c2c490f5ee3b406f80322789dbc0521
Sergio Mir <voidtracer6@gmail.com>
2023-07-10 13:06:02 -0400

    ./scr/bin/numeric-types.rs: types added

--- src/bin/numeric-types.rs
+++ src/bin/numeric-types.rs
@@ -3,12 +3,13 @@ fn sub(a: i32, b: i32) -> i32 {
 }
 
 fn main() {
-     let _sum = 2 + 2;
-     let _value = 10 - 5;
-     let _division = 10 / 2;
-     let _multiplication = 6 * 6;
+     let _sum: i32 = 2 + 2;
+     let _value: i32 = 10 - 5;
+     let _division: i32 = 10 / 2;
+     let _multiplication: i32 = 6 * 6;
+     let _rem: i32 = 6 % 4;
 
-     let five = sub(8 , 5);
+     let five: i32 = sub(8 , 5);
 
      println!("{:?}", five);
 }
