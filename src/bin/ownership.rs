4bf02d6775b1e8aec6f67c4d457a9cbbdd43f336
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 15:34:19 -0400

    ./scr/bin/ownership.rs: ADD implementation for the example of ownership

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -1,3 +1,22 @@
-fn main(){
+struct Book {
+    pages: i32,
+    rating: i32
+}
 
+fn display_page_count(book: &Book) {
+    println!("pages: {:?}", book.pages)
+}
+
+fn display_rating(book: &Book) {
+    println!("rating: {:?}", book.rating)
+}
+
+fn main(){
+    let my_book = Book {
+        pages: 380,
+        rating: 100
+   };
+
+    display_page_count(&my_book);
+    display_rating(&my_book);
 }
