03a307f8e9271860c53fd15dda721bdbd392c1fc
Sergio Mir <voidtracer6@gmail.com>
2023-07-10 21:38:05 -0400

    ./scr/bin/a5.rs: added

---
+++ src/bin/a5.rs
@@ -0,0 +1,23 @@
+// Topic: Looping using the loop statement
+//
+// Program requirements:
+// * Display "1" through  "4" in the terminal
+//
+// Notes:
+// * Use a mutable integer variable
+// * Use a loop statement
+// * Print the variable within the loop statement
+// * use a break to exit the loop
+
+fn main() {
+    let mut num: i32 = 1;
+
+    loop {
+        print!("{}", num);
+        num += 1;
+
+        if num == 4 {
+            break;
+        }
+    }
+}
