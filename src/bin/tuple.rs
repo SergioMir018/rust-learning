db9101ee07edbdc2cebad2c7a4d7dc4c2cb83b2c
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 10:03:21 -0400

    ./scr/bin/tuple.rs: new examples added

--- src/bin/tuple.rs
+++ src/bin/tuple.rs
@@ -4,4 +4,9 @@ fn example_tuple() -> (i32, i32, i32) {
 
 fn main() {
     let (a, b, c) = example_tuple();
+    let numbers = example_tuple();
+    
+    println!("destruct: {:?} index: {:?}", a, numbers.0);
+    println!("destruct: {:?} index: {:?}", b, numbers.1);
+    println!("destruct: {:?} index: {:?}", c, numbers.2);
 }
