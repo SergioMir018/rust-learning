438b08c19610f3a0c7c3147210178546acfadde7
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 12:29:21 -0400

    ./scr/bin/a9.rs: solution implemented

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -7,9 +7,19 @@
 // Notes:
 // * Use a boolean variable set to an expression that determines whether the value is
 // >100 or <= 100
-// * Use a function to 
+// * Use a function to print the messages
+// * Use a match expression to determine wich message to print
 
+fn eval_on_100(exp: bool) {
+    match exp {
+        true => print!("it's big"),
+        false => print!("it's small")
+    }
+}
 
 fn main() {
+    let number: i32 = 90;
+    let eval_exprs: bool = number > 100;
 
+    eval_on_100(eval_exprs);
 }
