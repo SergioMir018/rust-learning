0b3f99172404a6696a2b432e1edb448c16efb3ba
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 17:36:54 -0400

    ./scr/bin/impl.rs: MOD
    now using the impl on Temperature to add the function show_temp

--- src/bin/impl.rs
+++ src/bin/impl.rs
@@ -2,12 +2,14 @@ struct Temperature {
     degree_f: f64
 }
 
-fn show_temp(temp: Temperature) {
-    println!("{:?} dregrees F", temp.degree_f)
+impl Temperature {
+    fn show_temp(temp: Temperature) {
+        println!("{:?} dregrees F", temp.degree_f)
+    }
 }
 
 fn main() {
     let hot = Temperature { degree_f: 99.9 };
 
-    show_temp(hot);
+    Temperature::show_temp(hot);
 }
