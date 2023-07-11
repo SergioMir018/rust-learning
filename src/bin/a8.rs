21137d7d86484a9c0afff87dee5bf394f36157f9
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 10:16:25 -0400

    ./scr/bin/a8.rs: solution implemented

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -8,6 +8,18 @@
 // * Destructure the return value into two variables
 // * Use an if...else block to determine what to print 
 
-fn main() {
+fn build_coordinate(value_x: i32, value_y: i32) -> (i32, i32) {
+    (value_x, value_y)
+}
 
+fn main() {
+    let (_, y) = build_coordinate(1, 5);
+
+    if y > 5 {
+        print!("The value of coordinate y is greater than 5");
+    } else if y < 5 {
+        print!("The value of coordinate y is less than 5");
+    } else {
+        print!("The value of coordinate y is equal to 5");
+    }
 }
