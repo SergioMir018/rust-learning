3fbe44ee5b3988376c9d4c0edff92389ffad585f
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 16:03:37 -0400

    ./scr/bin/a10.rs: MOD
    the solution has been implemented

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -1,5 +1,33 @@
+// Topic: Ownership
+//
+// Program requirements:
+// * Print out the quantity and id number of a grocery item
+//
+// Notes:
+// * Use a struct for the grocery item
+// * Use two i32 fields for the quantity and id number
+// * Create a function to display the quantity
+// * Create a function to display the id number
 
+struct GroceryItem {
+    id: i32,
+    quantity: i32
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id)
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity)
+}
 
 fn main() {
+    let my_item = GroceryItem {
+        id: 3625,
+        quantity: 2
+    };
 
+    display_id(&my_item);
+    display_quantity(&my_item);
 }
