5fa4a18f7550c887621a5f49b361da5292472584
Sergio Mir <voidtracer6@gmail.com>
2023-07-11 12:12:12 -0400

    ./scr/bin/a9.rs: added

---
+++ src/bin/a9.rs
@@ -0,0 +1,15 @@
+// Topic: Working with expressions
+//
+// Requirements:
+// * Print "it's big" if a variable is > 100
+// * Print "it's small" if a variable is <= 100
+//
+// Notes:
+// * Use a boolean variable set to an expression that determines whether the value is
+// >100 or <= 100
+// * Use a function to 
+
+
+fn main() {
+
+}
