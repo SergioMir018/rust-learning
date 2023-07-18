2613a88ee44299dae9140ba53248bff4dd36503e
Serio_Mir <voidtracer6@gmail.com>
2023-07-18 10:41:43 -0400

    MOD
    string demo has been implemented

--- src/bin/string.rs
+++ src/bin/string.rs
@@ -1,8 +1,31 @@
 struct LineItem {
     name: String,
-    count: f32
+    count: i8
+}
+
+fn print_name(name: &str) {
+    println!("name: {}", name);
+}
+
+fn print_count(count: &i8) {
+    println!("count: {}", count);
 }
 
 fn main() {
+    let receipt = vec![
+        LineItem {
+            name: "cereal".to_owned(),
+            count: 1
+        }, 
+        LineItem {
+            name: String::from("fruit"),
+            count: 3
+        }
+    ];
 
+    for item in receipt {
+        print_name(&item.name);
+        print_count(&item.count);
+        println!("----------");
+    }
 }
