0357f113f14754a8407115e92d1843343390467e
Serio_Mir <voidtracer6@gmail.com>
2023-07-21 11:37:03 -0400

    ./scr/bin/hashmap.rs: MOD
    added the implementation for the demo

--- src/bin/hashmap.rs
+++ src/bin/hashmap.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Contents {
+    content: String,
+}
+
+impl Contents {
+    fn new(new_content: &str) -> Self {
+        Self {
+            content: String::from(new_content),
+        }
+    }
+}
+
 fn main() {
-    
+    let mut lockers: HashMap<i32, Contents> = HashMap::new();
+
+    lockers.insert(1, Contents::new("stuff"));
+    lockers.insert(2, Contents::new("shirt"));
+    lockers.insert(3, Contents::new("book"));
+
+    lockers.iter().for_each(|(key, content)| {
+        println!("number: {} content: {:?}", key, content);
+    })
 }
